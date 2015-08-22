6639380a4a9c9241efe64a44dfbb176f6bbfaf12
AmonKalagin <amon.kalagin@gmail.com>
2015-08-22 17:18:02 +0200

    Floor respwn added

---
+++ ColorsNStuff/Assets/EnvMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvMove : MonoBehaviour {
+
+	public float Speed;
+
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.position -= new Vector3(Speed, 0, 0);
+	}
+}
---
+++ ColorsNStuff/Assets/EnvSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvSpawn : MonoBehaviour {
+	public GameObject floorTile;
+	public GameObject spawn;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnTriggerExit2D (Collider2D col)
+	{
+		Instantiate(floorTile ,transform.position, Quaternion.identity);
+	}
+}
