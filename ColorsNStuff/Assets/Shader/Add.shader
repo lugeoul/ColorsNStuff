ad8c922f45e2c97a026aacb947e97686231d2c0d
unknown <event@2014-SK-22.games-academy.de>
2015-08-22 17:22:24 +0200

    added sprite animator for cat

---
+++ ColorsNStuff/Assets/Shader/Add.shader
@@ -0,0 +1,23 @@
+﻿Shader "Multiply" 
+{
+	SubShader 
+	{	
+		Pass
+		{
+			// use a pixel blend, so that it takes from the color from the buffer 
+			Blend SrcAlpha DstColor
+
+			// pixel blending only works with lighting, so turn it on
+			Lighting On
+			// make sure the depth gets written in
+			ZWrite On
+
+			// set the material color to invisible
+			Material
+			{
+				Diffuse(0,0,0,0)
+			}
+		}
+	} 
+	FallBack "Diffuse"
+}
---
+++ ColorsNStuff/Assets/SpriteAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimation : MonoBehaviour {
+
+    public Sprite[] walkAnimation;
+    SpriteRenderer sRenderer;
+    public float spriteFramesPerSecond;
+    float time = 0;
+    int animationIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        sRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if(time >= 1 / spriteFramesPerSecond)
+        {
+            time = 0;
+            animationIndex++;
+            if(animationIndex == walkAnimation.Length)
+                animationIndex = 0;
+            sRenderer.sprite = walkAnimation[animationIndex];
+        }
+    }
+}
